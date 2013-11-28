e2d52c13b486802dc8a605bd6f1955fcdfc7a440
Natasha Murashev <nmurashev@gmail.com>
2013-11-28 11:28:46 -0800

    initial commit

---
+++ NSTalks/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NSTalks
+//
+//  Created by Natasha Murashev on 11/28/13.
+//  Copyright (c) 2013 NatashaTheRobot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NSTalks/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NSTalks
+//
+//  Created by Natasha Murashev on 11/28/13.
+//  Copyright (c) 2013 NatashaTheRobot. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
